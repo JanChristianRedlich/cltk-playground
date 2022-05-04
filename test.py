d632661a38b82a23baab5ea8daf0d932e246acfc
Jan Christian Redlich <redlich@red-dinosaur.de>
2022-05-04 22:32:07 +0200

    initial commit (Quickstart)

---
+++ test.py
@@ -0,0 +1,4 @@
+from cltk import NLP
+vitruvius = "Architecti est scientia pluribus disciplinis et variis eruditionibus ornata, quae ab ceteris artibus perficiuntur. Opera ea nascitur et fabrica et ratiocinatione."
+cltk_nlp = NLP(language="lat")
+cltk_doc = cltk_nlp.analyze(text=vitruvius)
